0c816829a3d7ccff84c823ac23c2dd9b25daf910
Katherine639 <kway001@ucr.edu>
2019-01-31 05:14:44 -0800

    Update forktest.c

--- xv6/forktest.c
+++ xv6/forktest.c
@@ -1,6 +1,7 @@
 // Test that fork fails gracefully.
 // Tiny executable so that the limit can be filling the proc table.
 
+#include <limits.h>
 #include "types.h"
 #include "stat.h"
 #include "user.h"
@@ -25,24 +26,24 @@ forktest(void)
     if(pid < 0)
       break;
     if(pid == 0)
-      exit();
+        exit(0);
   }
 
   if(n == N){
     printf(1, "fork claimed to work N times!\n", N);
-    exit();
+      exit(0);
   }
 
   for(; n > 0; n--){
-    if(wait() < 0){
+    if(wait(0) < 0){
       printf(1, "wait stopped early\n");
-      exit();
+        exit(0);
     }
   }
 
-  if(wait() != -1){
+  if(wait(0) != -1){
     printf(1, "wait got too many\n");
-    exit();
+      exit(0);
   }
 
   printf(1, "fork test OK\n");
@@ -52,5 +53,5 @@ int
 main(void)
 {
   forktest();
-  exit();
+    exit(0);
 }
